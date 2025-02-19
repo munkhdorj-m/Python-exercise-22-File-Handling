55a3c79fe4254f678f1e327008cfcce1d8990570
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-02-20 00:45:10 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,16 +1,21 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def extract_emails(string):
+def create_and_read_file(filename: str) -> None:
     # Write your code here
     pass
 
 # Exercise 2
-def mask_credit_card_numbers(string):
+def write_user_info(filename: str, name: str, age: int) -> None:
     # Write your code here
     pass
 
 # Exercise 3
-def find_duplicate_characters(string):
+def count_words_in_file(filename: str) -> int:
+    # Write your code here
+    pass
+
+# Exercise 4
+def find_longest_word_in_file(filename: str) -> str:   
     # Write your code here
     pass
